d27b695ca9ad714840ed7aaed0d9ca7b19106438
Ben Nizette <benjamin.nizette@anu.edu.au>
2015-07-21 10:23:43 +1000

    Wire demo and reset buttons to ajax

--- fpga_dash/ajax.py
+++ fpga_dash/ajax.py
@@ -12,3 +12,13 @@ def do_switches(request, state):
 def do_key(request, id, state):
 	print "key %d in state %d" % (int(id), int(state))
 	return json.dumps({'result' : 'ok'})
+
+@dajaxice_register
+def do_reset(request):
+	print "resetting board"
+	return json.dumps({'result' : 'ok'})
+
+@dajaxice_register
+def do_demo(request):
+	print "loading demo"
+	return json.dumps({'result' : 'ok'})
