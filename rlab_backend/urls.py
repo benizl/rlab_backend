3a3ef0f021bd0d3dc31021fb33361eace060dd54
Ben Nizette <benjamin.nizette@anu.edu.au>
2015-08-13 09:27:44 +1000

    Urls: Whitespace change

--- rlab_backend/urls.py
+++ rlab_backend/urls.py
@@ -7,7 +7,7 @@ dajaxice_autodiscover()
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.redirect_to_dash, name='redirect'),
+    url(r'^$', views.redirect_to_dash, name='redirect'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^dashboard/', include('fpga_dash.urls', namespace='dashboard')),
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
